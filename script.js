5f832461b489ee053181f1e1326d79c5e7f3896d
Matheus <124745048+Matheuzzzzzzz@users.noreply.github.com>
2026-08-17 21:13:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Alterna entre modo claro e escuro ao clicar no botão do menu
+document.getElementById('dark-mode-button').addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+});
